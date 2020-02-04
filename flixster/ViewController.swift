79461be1e69319e47b9f510247e478a92299eab1
Gurpreet Sidhu <gsidhu@csumb.edu>
2020-02-03 17:07:01 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Gurpreet Sidhu on 2/3/20.
+//  Copyright © 2020 gsidhu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
